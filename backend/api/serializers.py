f3f2eaee4628dfcd7e6551f831678fb2ec1369e5
Belotserkovets Mikhail <ren4ik92@gmail.com>
2023-04-21 21:45:13 +0300

    rev

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -65,26 +65,6 @@ class RecipeSerializer(serializers.ModelSerializer):
             return False
         return Recipe.objects.filter(cart__user=user, id=obj.id).exists()
 
-    # def validate(self, data):
-    #     ingredients = self.initial_data.get('ingredients')
-    #     if not ingredients:
-    #         raise serializers.ValidationError({
-    #             'ingredients': 'Нужен хоть один ингридиент для рецепта'})
-    #     ingredient_list = []
-    #     for ingredient_item in ingredients:
-    #         ingredient = get_object_or_404(Ingredient,
-    #                                        id=ingredient_item['id'])
-    #         if ingredient in ingredient_list:
-    #             raise serializers.ValidationError('Ингридиенты должны '
-    #                                               'быть уникальными')
-    #         ingredient_list.append(ingredient)
-    #         if int(ingredient_item['amount']) < 0:
-    #             raise serializers.ValidationError({
-    #                 'ingredients': ('Убедитесь, что значение количества '
-    #                                 'ингредиента больше 0')
-    #             })
-    #     data['ingredients'] = ingredients
-    #     return data
     def validate_ingredients(self, ingredients):
         ingredient_list = []
 
@@ -193,4 +173,3 @@ class FollowSerializer(serializers.ModelSerializer):
 
     def get_recipes_count(self, obj):
         return Recipe.objects.filter(author=obj.author).count()
-
