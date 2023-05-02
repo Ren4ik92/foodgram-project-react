51fb687de791fb15a26eb297228f561cb310e8bd
Belotserkovets Mikhail <ren4ik92@gmail.com>
2023-05-02 13:14:57 +0300

    valid

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -22,7 +22,6 @@ class ReadRecipeIngredientSerializer(serializers.ModelSerializer):
     name = serializers.ReadOnlyField(source='ingredient.name', read_only=True)
     measurement_unit = serializers.ReadOnlyField(
         source='ingredient.measurement_unit', read_only=True)
-    # amount = serializers.IntegerField()
 
     class Meta:
         fields = ('id', 'name', 'measurement_unit', 'amount')
@@ -33,7 +32,6 @@ class CreateRecipeIngredientSerializer(serializers.ModelSerializer):
     id = serializers.PrimaryKeyRelatedField(
         queryset=Ingredient.objects.all(),
     )
-    # amount = serializers.IntegerField()
 
     class Meta:
         fields = ('id', 'amount',)
@@ -151,15 +149,22 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
         if data.get('cooking_time') < 1:
             raise serializers.ValidationError(
                 'Время приготовления должно быть не меньше одной минуты!')
-        ingredients = data.get('ingredients')
-        tags = data.get('tags')
+
+        self.validate_ingredients(data.get('ingredients'))
+        return data
+
+    def validate_ingredients(self, ingredients):
         if not ingredients or len(ingredients) == 0:
             raise serializers.ValidationError(
                 'Рецепт должен содержать хотя бы один ингредиент!')
+        return ingredients
+
+    def validate_tags(self, data):
+        tags = data.get('tags')
         if not tags or len(tags) == 0:
             raise serializers.ValidationError(
                 'Рецепт должен содержать хотя бы один тег!')
-        return data
+        return tags
 
 
 class ReadRecipeSerializer(serializers.ModelSerializer):
