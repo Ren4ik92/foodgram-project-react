86d1495fafdba886a90018a1d36d816db4a59a05
Belotserkovets Mikhail <ren4ik92@gmail.com>
2023-05-03 11:05:33 +0300

    fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -71,12 +71,12 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
         ingredients = validated_data.pop('ingredients')
         author = validated_data.pop('author', current_user)
         recipe = Recipe.objects.create(author=author, **validated_data)
-        ingredient_ids = set()
-        for ingredient in ingredients:
-            ingredient_id = ingredient['id'].id
-            if ingredient_id in ingredient_ids:
-                raise serializers.ValidationError('Каждый ингредиент должен присутствовать только один раз')
-            ingredient_ids.add(ingredient_id)
+        # ingredient_ids = set()
+        # for ingredient in ingredients:
+        #     ingredient_id = ingredient['id'].id
+        #     if ingredient_id in ingredient_ids:
+        #         raise serializers.ValidationError('Каждый ингредиент должен присутствовать только один раз')
+        #     ingredient_ids.add(ingredient_id)
 
         ingredient_amounts = []
         for ingredient in ingredients:
@@ -125,6 +125,14 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
         if not ingredients or len(ingredients) == 0:
             raise serializers.ValidationError(
                 'Рецепт должен содержать хотя бы один ингредиент!')
+
+        ingredient_ids = set()
+        for ingredient in ingredients:
+            ingredient_id = ingredient['id'].id
+            if ingredient_id in ingredient_ids:
+                raise serializers.ValidationError('Каждый ингредиент должен присутствовать только один раз')
+            ingredient_ids.add(ingredient_id)
+
         return data
 
 
